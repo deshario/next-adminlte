9b9189fa4a14b0d128ab7bf7944c4c4a1267c1e4
deshario <deshario9@gmail.com>
2020-10-12 15:05:50 +0700

    UI Modified

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,14 @@
 import AdminHOC from '../components/layouts/admin.hoc';
 
-const Home = () => (
-  <AdminHOC contentTitle="Home">
+const Devices = () => (
+  <AdminHOC contentTitle="Devices">
    <div className="container-fluid">
       <div className="row">
         <div className="col-lg-12">
    
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Welcome to e-voting manager</h5>
+              <h5 className="card-title">Welcome to device manager</h5>
               <p className="card-text">
                
               </p>
@@ -21,4 +21,4 @@ const Home = () => (
   </AdminHOC>
 )
 
-export default Home
+export default Devices
--- client/pages/owners.js
+++ client/pages/owners.js
@@ -1,14 +1,14 @@
 import AdminHOC from '../components/layouts/admin.hoc';
 
-const MyElection = () => (
-  <AdminHOC contentTitle="MyElection">
+const Owners = () => (
+  <AdminHOC contentTitle="Owners">
    <div className="container-fluid">
       <div className="row">
         <div className="col-lg-12">
 
           <div className="card card-default">
             <div className="card-header">
-              <h3 className="card-title">MyElection</h3>
+              <h3 className="card-title">Owners</h3>
             </div>
             <div className="card-body">
               <div className="row">
@@ -22,4 +22,4 @@ const MyElection = () => (
   </AdminHOC>
 )
 
-export default MyElection
+export default Owners
